d1c683094936d2f265fed65b0a385fd6be7d7858
allamalitskad <allamalitskad@gmail.com>
2022-01-16 15:33:56 +0200

    ...

--- store/store/settings.py
+++ store/store/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-mi&+e(oqa+(&71e@mdrvb!3yh8(+kxkmvl188*pnu-i1psm$@s'
+SECRET_KEY = '/*****changeme######'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
